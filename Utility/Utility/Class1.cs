66bab8d8a80d6973e481d39a229e2c93119c87f8
M. UTKU KUL <m.utkukul@gmail.com>
2020-07-19 02:00:00 +0300

    Cryptography

--- Utility/Utility/Class1.cs
+++ Utility/Utility/Class1.cs
@@ -1,8 +1,40 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Utility
 {
-    public class Class1
+    public static class Cryptography
     {
+        private static string hash = "placeputyourownstringkey";
+        public static string Encrypt(string password)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(password);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        public static string Decrypt(string hashedValue)
+        {
+            byte[] data = Convert.FromBase64String(hashedValue);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
     }
 }
