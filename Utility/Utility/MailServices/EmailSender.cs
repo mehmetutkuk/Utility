5b3358cd7fd2ad6e798f0d858be6f2a71eaee388
M. UTKU KUL <m.utkukul@gmail.com>
2020-08-03 17:39:36 +0300

    EmailServices

---
+++ Utility/Utility/MailServices/EmailSender.cs
@@ -0,0 +1,106 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Utility.Models;
+
+namespace Utility.MailServices
+{
+
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailType emailType);
+        public string[] SetMailBoxes { set; }
+    }
+    public enum EmailType
+    {
+        passwordReset,
+        activationCode
+    }
+
+    public class EmailService : IEmailService
+    {
+        private MimeMessage emailMessage;
+
+        private static IHostingEnvironment _hostingEnvironment;
+
+        private static string Subject(EmailType type)
+        {
+            switch (type)
+            {
+                case EmailType.passwordReset:
+                    return "Password Reset";
+                case EmailType.activationCode:
+                    return "Activate Your Account";
+                default:
+                    return "Activate Your Account";
+            }
+        }
+
+        private static string HtmlBody(EmailType type)
+        {
+            string path = Directory.GetCurrentDirectory();
+            switch (type)
+            {
+                case EmailType.passwordReset:
+                    path = Path.Combine(path,"MailServices/HtmlThemes/passwordReset.html");
+                    break;
+                case EmailType.activationCode:
+                    path = Path.Combine(path, "MailServices/HtmlThemes/activation.html");
+                    break;
+                default:
+                    path = Path.Combine(path, "MailServices/HtmlThemes/activation.html");
+                    break;
+            }
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private readonly EmailConfig ec;
+
+        public string[] SetMailBoxes
+        {
+            set { emailMessage.To.Add(new MailboxAddress(value[0], value[1])); }
+        }
+
+        public EmailService(IOptions<EmailConfig> emailConfig, IHostingEnvironment environment)
+        {
+            emailMessage = new MimeMessage();
+            this.ec = emailConfig.Value;
+            _hostingEnvironment = environment;
+        }
+
+        public async Task SendEmailAsync(EmailType emailType)
+        {
+            try
+            {
+                emailMessage.From.Add(new MailboxAddress(ec.FromName, ec.FromAddress));
+                emailMessage.Subject = Subject(emailType);
+                emailMessage.Body = new TextPart(TextFormat.Html) { Text = HtmlBody(emailType) };
+                using (var client = new SmtpClient())
+                {
+                    client.LocalDomain = ec.LocalDomain;
+
+                    await client.ConnectAsync(ec.MailServerAddress, Convert.ToInt32(ec.MailServerPort), SecureSocketOptions.Auto).ConfigureAwait(false);
+                    await client.AuthenticateAsync(new NetworkCredential(ec.UserId, ec.UserPassword));
+                    await client.SendAsync(emailMessage).ConfigureAwait(false);
+                    await client.DisconnectAsync(true).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Utility/Utility/Models/EmailConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Utility.Models
+{
+    public class EmailConfig
+    {
+        public String FromName { get; set; }
+        public String FromAddress { get; set; }
+
+        public String LocalDomain { get; set; }
+
+        public String MailServerAddress { get; set; }
+        public String MailServerPort { get; set; }
+
+        public String UserId { get; set; }
+        public String UserPassword { get; set; }
+    }
+}
